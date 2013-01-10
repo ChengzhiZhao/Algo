358949e46a01566297843b5b04a85858c69045e4
Longwei Su <lsu@ics.com>
2013-01-10 12:03:30 -0500

    move append to different function
    I missed the C++reference

--- src/ch2/Partition.java
+++ src/ch2/Partition.java
@@ -1,7 +1,7 @@
 package ch2;
 import CtCILibrary.*;
 
-/*
+/*  2.4
  *  partition a linked list so that all node less than x come before all nodes greater or larger than
  */
 
@@ -16,50 +16,84 @@ public class Partition {
 		LinkedListNode current = head;
 		for (int i = 1; i < vals.length; i++) {
 			current = new LinkedListNode(vals[i], null, current);
-		}
-		System.out.println(head.printForward());
-		
+		}		
 		/* Partition */
-		LinkedListNode h = partition(head, 5);
+//		LinkedListNode h = partition(head, 5);
+		LinkedListNode h = part(head, 5);
 		
 		/* Print Result */
 		System.out.println(h.printForward());
 
 	}
 	
-	public static LinkedListNode partition(LinkedListNode node, int x){
+	public static LinkedListNode partition(LinkedListNode head, int x){
 		LinkedListNode beforeHead = null;
 		LinkedListNode beforeTail = null;
 		LinkedListNode afterHead = null;
 		LinkedListNode afterTail = null;
 		
-		while(node != null){
-			LinkedListNode next = node.next;
-			node.next = null;
-			if(node.data < x){
+		while(head != null){
+			LinkedListNode next = head.next;
+			head.next = null;
+			if(head.data < x){
 				//append to before
 				if(beforeHead == null){ 
-					beforeHead = node;
-					beforeTail = node;
+					beforeHead = head;
+					beforeTail = head;
 					}else{
-						beforeTail.next = node;
-						beforeTail = node;
+						beforeTail.next = head;
+						beforeTail = head;
 					}
 			} else{
 				//append to after
 				if(afterHead == null){
-					afterHead = node;
-					afterTail = node;
+					afterHead = head;
+					afterTail = head;
 				}else{
-					afterTail.next = node;
-					afterTail = node;
+					afterTail.next = head;
+					afterTail = head;
 				}
 			}
-			node = next;
+			head = next;
 		}
 		//merge
 		beforeTail.next = afterHead;
 		return beforeHead;
 	}
-
+	
+	
+	/*
+	 * move append to different function
+	 */
+	public static LinkedListNode part(LinkedListNode head, int x){
+		LinkedListNode[] first = {null, null};//0 for head, 1 for tail
+		LinkedListNode[] second = {null, null};
+		while(head != null){
+			LinkedListNode cur = head;
+			head = head.next;
+			cur.next = null;//disconnect
+			if(cur.data < x){
+				first = append(first[0], first[1], cur);
+			} else {//append to after
+				second = append(second[0], second[1], cur);
+			}	
+		}
+		first[1].next = second[0];
+		return first[0];
+	}
+	
+	public static LinkedListNode[] append(LinkedListNode head, LinkedListNode tail, LinkedListNode node){
+		assert(node != null);
+		if(tail == null){
+			assert(head == null);
+			head = node;
+			tail = node;
+		}else{
+			tail.next = node;
+			tail = node;
+		}
+		LinkedListNode[] ret = {head, tail};
+		return ret;
+	}
+	
 }
