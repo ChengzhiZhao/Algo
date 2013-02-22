abb285c907c03597e410ac8a9ea37a15ce054766
longwei <longwei.su@gmail.com>
2013-02-22 17:28:18 -0500

    17

--- src/algoHw/Graph.java
+++ src/algoHw/Graph.java
@@ -1,15 +1,18 @@
 package algoHw;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Graph {
-	private final int V;
 	private Map<Integer, Vertex> adj;;
-	private Set<Edge> edges = new HashSet<Edge>();
+	private List<Edge> edges = new ArrayList<Edge>();
 
 	private static class Vertex {
 		private int lbl;
@@ -36,6 +39,11 @@ public class Graph {
 		public String toString() {
 			return adjv.toString();
 		}
+
+		public boolean contains(int i) {
+			return adjv.contains(i);
+			// return false;
+		}
 	}
 
 	static class Edge {
@@ -57,42 +65,43 @@ public class Graph {
 
 		public boolean equals(Object c) {
 			Edge other = (Edge) c;
-			return (other.first == this.first && other.second == this.second)
-					|| (other.second == this.first && other.first == this.second);
+			return other.first == this.first && other.second == this.second;
 		}
 
 		public int hashCode() {
-			int max = Math.max(first, second);
-			int min = Math.min(first, second);
-			return max * 31 + min;
+			return first * 31 + second;
 		}
 	}
 
 	public Graph(int[][] arr) {
-		V = arr.length;
+		// V = arr.length;
 		adj = new TreeMap<Integer, Vertex>();
+		for (int[] row : arr) {
+			adj.put(row[0], new Vertex(row[0]));
+		}
+
 		for (int[] row : arr) {
 			if (row != null) {
 				for (int i = 1; i < row.length; ++i) {
-					addEdge(row[0], row[i]);
+					if (!adj.get(row[i]).contains(row[0])) {
+						addEdge(row[0], row[i]);
+					}
 				}
 			}
 		}
 	}
 
-	public int V() {
-		return V;
-	}
-
 	public void addEdge(int v, int w) {
+		Edge e = new Edge(v, w);
+		edges.add(e);
 		if (adj.get(v) == null) {
 			adj.put(v, new Vertex(v));
 		}
-		adj.get(v).addV(w);
-		Edge e = new Edge(v, w);
-		if (!edges.contains(e)) {
-			edges.add(e);
+		if (adj.get(w) == null) {
+			adj.put(w, new Vertex(w));
 		}
+		adj.get(v).addV(w);
+		adj.get(w).addV(v);
 	}
 
 	public Vertex adj(int v) {
@@ -100,9 +109,13 @@ public class Graph {
 	}
 
 	public void rmEdge(int v, int w) {
-		if (!edges.contains(new Edge(v, w)))
+		// System.out.println("-: " + v + ":" + w);
+		if (!edges.contains(new Edge(v, w)) && !edges.contains(new Edge(w, v)))
 			throw new IllegalArgumentException("invalid edge");
-		edges.remove(new Edge(v, w));
+		if (!edges.remove(new Edge(v, w))) {
+			edges.remove(new Edge(w, v));
+		}
+		;
 		Vertex lv = adj.get(v);
 		lv.rmV(w);
 		Vertex lw = adj.get(w);
@@ -110,11 +123,23 @@ public class Graph {
 	}
 
 	public void fuse(int from, int to) {
+		Vertex vfrom = adj.get(from);
+		int[] list = new int[vfrom.adjv.size()];
+		for (int i = 0; i < list.length; ++i) {
+			list[i] = vfrom.adjv.get(i);
+		}
+		for (int i = 0; i < list.length; ++i) {
+			int other = list[i];
+			this.rmEdge(from, other);
+			if (to != other)
+				this.addEdge(to, other);
+		}
+		adj.remove(from);
 	}
 
 	public String toString() {
-		StringBuffer str = new StringBuffer("V: " + V + " E: " + edges.size()
-				+ "\n");
+		StringBuffer str = new StringBuffer("V: " + adj.size() + " E: "
+				+ edges.size() + "\n");
 		for (Map.Entry<Integer, Vertex> entry : adj.entrySet()) {
 			int v = entry.getKey();
 			Vertex list = entry.getValue();
@@ -124,19 +149,61 @@ public class Graph {
 		return str.toString();
 	}
 
-	public static void main(String[] args) {
-		int[][] input = { { 1, 2, 5, 6 }, { 2, 1, 5, 6, 3 }, { 3, 2, 4, 7, 8 },
-				{ 4, 3, 7, 8 }, { 5, 1, 2, 6 }, { 6, 1, 2, 5, 7 },
-				{ 7, 3, 6, 8, 4 }, { 8, 3, 4, 7 } };
-		Graph g = new Graph(input);
-		// System.out.println(g.toString());
-		// g.rmEdge(new Edge(1, 5));
-		g.rmEdge(1, 5);
-		// g.addEdge(2, 5);
-		// g.addEdge(5, 6);
-		// g.replace(1, 5);
-		System.out.println(g.toString());
+	public static int minCut(Graph g) {
+		Random rnd = new Random();
+		while (g.adj.size() > 2) {
+			int index = rnd.nextInt(g.edges.size());
+			Edge e = g.edges.get(index);
+			g.fuse(e.first, e.second);
+		}
+		System.out.println(g.edges.size());
+		return g.edges.size();
+	}
 
+	public static void main(String[] args) {
+		// int[][] arr = { { 1, 2, 5, 6 }, { 2, 1, 5, 6, 3 }, { 3, 2, 4, 7, 8 },
+		// { 4, 3, 7, 8 }, { 5, 1, 2, 6 }, { 6, 1, 2, 5, 7 },
+		// { 7, 3, 6, 8, 4 }, { 8, 3, 4, 7 } };
+		int[][] arr = read("input/kargerMinCut.txt");
+		Map<Integer, Integer> statistics = new LinkedHashMap<Integer, Integer>();
+		int min = arr.length;
+//		int iter = arr.length * arr.length;
+		int iter = 20;
+		for (int i = 0; i < iter; i++) {
+			Graph gr = new Graph(arr);
+			int currMin = minCut(gr);
+			min = Math.min(min, currMin);
+
+			Integer counter;
+			if ((counter = statistics.get(currMin)) == null) {
+				counter = 0;
+			}
+			statistics.put(currMin, counter + 1);
+		}
+		System.out.println("Min: " + min + " stat: "
+				+ (statistics.get(min) * 100 / iter) + "%");
+
+	}
+
+	public static int[][] read(String filename) {
+		int[][] ret = new int[200][];
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File(filename));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (scanner.hasNextLine()) {
+			String[] tmp = scanner.nextLine().split("(\\s)+");
+			int[] row = new int[tmp.length];
+			for (int i = 0; i < tmp.length; ++i) {
+				row[i] = Integer.parseInt(tmp[i]);
+			}
+			ret[row[0] - 1] = row;
+		}
+		scanner.close();
+		return ret;
 	}
 
 }
