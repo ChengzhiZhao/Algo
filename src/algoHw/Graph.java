cda584ef4f0cd9bfaf9b039a68757cae13960779
Longwei Su <longwei.su@gmail.com>
2013-02-21 23:53:33 -0500

    for graph, after initialization, only addEdge and rmEdge ops

--- src/algoHw/Graph.java
+++ src/algoHw/Graph.java
@@ -8,8 +8,35 @@ import java.util.TreeMap;
 
 public class Graph {
 	private final int V;
-	private Map<Integer, Vertex> adj = new TreeMap<Integer, Vertex>();
+	private Map<Integer, Vertex> adj;;
 	private Set<Edge> edges = new HashSet<Edge>();
+
+	private static class Vertex {
+		private int lbl;
+		private ArrayList<Integer> adjv;
+
+		public Vertex(int lbl) {
+			this.lbl = lbl;
+			adjv = new ArrayList<Integer>();
+		}
+
+		public void addV(int v2) {
+			adjv.add(v2);
+		}
+
+		public void rmV(int v2) {
+			for (int i = 0; i < adjv.size(); ++i) {
+				if (adjv.get(i) == v2) {
+					adjv.remove(i);
+					return;
+				}
+			}
+		}
+
+		public String toString() {
+			return adjv.toString();
+		}
+	}
 
 	static class Edge {
 		private int first;
@@ -27,30 +54,27 @@ public class Graph {
 		public String toString() {
 			return new String(first + "-" + second);
 		}
-		
-		public boolean equals(Object c){
-			Edge other = (Edge)c;
-			return (other.first == this.first && other.second == this.second) || 
-					(other.second == this.first && other.first == this.second);
+
+		public boolean equals(Object c) {
+			Edge other = (Edge) c;
+			return (other.first == this.first && other.second == this.second)
+					|| (other.second == this.first && other.first == this.second);
 		}
-		public int hashCode(){
+
+		public int hashCode() {
 			int max = Math.max(first, second);
 			int min = Math.min(first, second);
-			return max*31 + min;
+			return max * 31 + min;
 		}
 	}
 
 	public Graph(int[][] arr) {
 		V = arr.length;
+		adj = new TreeMap<Integer, Vertex>();
 		for (int[] row : arr) {
 			if (row != null) {
-				Vertex thisRow = new Vertex(row);
-				adj.put(row[0], thisRow);
-				for(int edgeTo: thisRow.adjv){
-					Edge e = new Edge(row[0], edgeTo);
-					if (!edges.contains(e)){
-						edges.add(new Edge(row[0], edgeTo));
-					}
+				for (int i = 1; i < row.length; ++i) {
+					addEdge(row[0], row[i]);
 				}
 			}
 		}
@@ -59,33 +83,33 @@ public class Graph {
 	public int V() {
 		return V;
 	}
-	
+
 	public void addEdge(int v, int w) {
+		if (adj.get(v) == null) {
+			adj.put(v, new Vertex(v));
+		}
 		adj.get(v).addV(w);
+		Edge e = new Edge(v, w);
+		if (!edges.contains(e)) {
+			edges.add(e);
+		}
 	}
 
 	public Vertex adj(int v) {
 		return adj.get(v);
 	}
-	
-	public void rmEdge(Edge e){
-		edges.remove(e);
+
+	public void rmEdge(int v, int w) {
+		if (!edges.contains(new Edge(v, w)))
+			throw new IllegalArgumentException("invalid edge");
+		edges.remove(new Edge(v, w));
+		Vertex lv = adj.get(v);
+		lv.rmV(w);
+		Vertex lw = adj.get(w);
+		lw.rmV(v);
 	}
-	public void replace(int from, int to){
-		Vertex fixingIndex = adj.get(from);
-		for(int i: fixingIndex.adjv){
-			adj.get(i).replace(from, to);
-		}
-		Vertex adding = adj.get(from);
-		for(int i: adding.adjv){
-			adj.get(to).addV(i);
-			System.out.println();
-		}
-		for(int i: adj.get(to){
-			if(i == to){
-				adj.remove(i);
-			}
-		}
+
+	public void fuse(int from, int to) {
 	}
 
 	public String toString() {
@@ -100,65 +124,17 @@ public class Graph {
 		return str.toString();
 	}
 
-	private static class Vertex {
-		private final int lbl;
-		private ArrayList<Integer> adjv;
-
-		public Vertex(int lbl) {
-			this.lbl = lbl;
-			adjv = new ArrayList<Integer>();
-		}
-
-		public void replace(int from, int to) {
-			// TODO Auto-generated method stub
-//			for(int i: adjv){
-//				if(i == from){
-//					adjv.remove(i);
-//					if(to != lbl) adjv.add(to);
-//				}
-//			}
-			for(int i=0; i < adjv.size(); ++i){
-				if(adjv.get(i) == from){
-					adjv.remove(i);
-//					if(to != lbl) 
-					adjv.add(to);
-				}
-			}
-		}
-
-		public boolean contains(int i) {
-			// TODO Auto-generated method stub
-			return adjv.contains(i);
-		}
-
-		public Vertex(int[] arr) {
-			this(arr[0]);
-			for (int i = 1; i < arr.length; ++i) {
-				adjv.add(arr[i]);
-			}
-		}
-
-		public void addV(int v2) {
-			adjv.add(v2);
-		}
-
-		public String toString() {
-			return adjv.toString();
-		}
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[][] input = { { 1, 2, 5, 6 }, { 2, 1, 5, 6, 3 }, { 3, 2, 4, 7, 8 },
 				{ 4, 3, 7, 8 }, { 5, 1, 2, 6 }, { 6, 1, 2, 5, 7 },
-				{ 7, 3, 6, 8, 4 }, { 8, 4, 7 } };
+				{ 7, 3, 6, 8, 4 }, { 8, 3, 4, 7 } };
 		Graph g = new Graph(input);
-//		System.out.println(g.toString());
-//		g.rmEdge(new Edge(1, 5));
-		g.rmEdge(new Edge(1, 5));
-//		g.addEdge(2, 5);
-//		g.addEdge(5, 6);
-		g.replace(1, 5);
+		// System.out.println(g.toString());
+		// g.rmEdge(new Edge(1, 5));
+		g.rmEdge(1, 5);
+		// g.addEdge(2, 5);
+		// g.addEdge(5, 6);
+		// g.replace(1, 5);
 		System.out.println(g.toString());
 
 	}
